b6ceabe8f97068f13c31417e3e6694f2435ddc1a
haraldr <fridrich1900@gmail.com>
2023-11-11 19:29:27 +0100

    attack & pikes

--- battle_units/ru_infantry.gd
+++ battle_units/ru_infantry.gd
@@ -7,16 +7,22 @@ var selected = false
 var fire = true
 var sprite = NAN
 var firing = NAN
+var is_player = false
+
+
+# Mechanics
+var HealthPoints = 100
+var Discipline = 100
+var DistanceDamage = 15
 
 signal enemy_clicked(click_position)
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == BUTTON_RIGHT:
-			var click_position = get_global_mouse_position()
-			var collision = get_world_2d().direct_space_state.intersect_point(click_position)
-			if collision.size() > 0 and collision[0]["collider"] == self:
-				emit_signal("enemy_clicked", click_position)
+			click_position = get_global_mouse_position()
+			if check_clicked(click_position):
+				emit_signal("enemy_clicked", position)
 				print("ru clicked")
 
 func _ready():
@@ -27,21 +33,27 @@ func _ready():
 	firing.play("none")
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("left_click") and selected:
-		click_position = get_global_mouse_position()
-		selected = false
+	if is_player:
+		if selected and Input.is_action_just_pressed("left_click"):
+			click_position = get_global_mouse_position()
+			selected = false
+			
+			sprite.play("default")
+			
+		var target_position = (click_position - position).normalized()
 		
-		sprite.play("default")
-		
-	var target_position = (click_position - position).normalized()
+		if position.distance_to(click_position) > 50:
+			move_and_slide(target_position * speed)
+		if fire:
+			firing.play("firing")
+		else:
+			firing.play("non")
 	
-	if position.distance_to(click_position) > 50:
-		move_and_slide(target_position * speed)
-	if fire:
-		firing.play("firing")
-	else:
-		firing.play("non")
+	mechanics()
 
+func mechanics():
+	if HealthPoints == 0:
+		print("Dead!!!")
 
 func _on_Button_pressed():
 	selected = !selected
@@ -49,3 +61,14 @@ func _on_Button_pressed():
 		sprite.play("default_selected")
 	else:
 		sprite.play("default")
+
+func get_damaged(dm):
+	HealthPoints -= dm
+	if HealthPoints < 0:
+		HealthPoints = 0
+	
+func check_clicked(pos):
+	var collision = get_world_2d().direct_space_state.intersect_point(pos)
+	if collision.size() > 0 and collision[0]["collider"] == self:
+		return true
+	return false
--- battle_units/sw_infantry.gd
+++ battle_units/sw_infantry.gd
@@ -5,26 +5,44 @@ var velocity = Vector2(0, 0)
 var click_position = Vector2(0, 0)
 var enemy_position = Vector2(0, 0)
 var selected = false
-var attack = false
+var fire_attack = false
 var fire = false
+var pikes = true
+
+# sprites
 var sprite = NAN
 var firing_sprite = NAN
+var pikes_sprite = NAN
 
 var fire_time_1: float = 0
 var fire_time_2: float = 6
 
-signal do_damage(dm)
+signal do_damage(dm, pos)
 
 func _ready():
 	click_position = Vector2(position.x, position.y)
 	sprite = $pictogram
 	firing_sprite = $firing_sprite
+	pikes_sprite = $pikes_sprite
 	sprite.play("default")
 	firing_sprite.play("none")
+	pikes_sprite.play("default")
 	
 
 func _physics_process(delta):
 	fire_time_2 += delta
+	slection_mechanic()
+	
+	movment_mechanic(delta)
+	
+	attack_mechanic()
+
+
+
+
+
+
+func slection_mechanic():
 	if Input.is_action_just_pressed("left_click") and selected:
 		click_position = get_global_mouse_position()
 		selected = false
@@ -33,41 +51,55 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("right_click") and selected:
 		selected = false
 		sprite.play("default")
+		
+	if Input.is_action_just_pressed("1") and selected:
+		pikes = false
+		pikes_sprite.play("default")
 	
-	var target_position = (click_position - position).normalized()
-	
-	
-	if attack:
-		if position.distance_to(click_position) > 300:
-			var target_rotation_degree = rad2deg(target_position.angle()) + 90
-			if abs(target_rotation_degree) < 90:
-				rotation_degrees = lerp(rotation_degrees, target_rotation_degree, 2.0 * delta)
-			move_and_slide(target_position * speed)
-		else:
-			attack = false
-			fire = true
-			click_position = position
-	else:
-		if position.distance_to(click_position) > 50:
-			var target_rotation_degree = rad2deg(target_position.angle()) + 90
-			if abs(target_rotation_degree) < 90:
-				rotation_degrees = lerp(rotation_degrees, target_rotation_degree, 2.0 * delta)
-			move_and_slide(target_position * speed)
-			fire = false
-	
-	if fire and position.distance_to(click_position) < 310:
+	if Input.is_action_just_pressed("2") and selected:
+		fire = false
+		fire_attack = false
+		pikes = true
+		pikes_sprite.play("pikes")
+
+func attack_mechanic():
+	if fire: # and position.distance_to(click_position) < 310
 		if fire_time_2-fire_time_1 > 6:
-			emit_signal("do_damage", 20)
+			emit_signal("do_damage", 20, enemy_position)
 			fire_time_2 = 0
 		firing_sprite.play("firing")
 	else:
 		firing_sprite.play("none")
 
+func movment_mechanic(delta):
+	if fire_attack:
+		if position.distance_to(enemy_position) > 300:
+			var target_position = (enemy_position - position).normalized()
+			unit_movement(target_position, delta)
+		else:
+			fire = true
+	else:
+		if position.distance_to(click_position) > 50:
+			var target_position = (click_position - position).normalized()
+			unit_movement(target_position, delta)
+			fire = false
+
+func unit_movement(target_position, delta):
+	var target_rotation_degree = rad2deg(target_position.angle()) + 90
+	if abs(target_rotation_degree) < 90:
+		rotation_degrees = lerp(rotation_degrees, target_rotation_degree, 2.0 * delta)
+	move_and_slide(target_position * speed)
+
+
+
+# logic
+
 func _on_enemy_clicked(pos):
-	print("Player clicked on an enemy.")
-	click_position = pos
-	attack = true
-	
+	if selected:
+		print("Player clicked on an enemy.")
+		enemy_position = pos
+		fire_attack = true
+
 
 func _on_Button_pressed():
 	selected = !selected
